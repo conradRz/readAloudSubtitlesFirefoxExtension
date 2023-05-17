9e9b4ee66d7f4e759a17e209c1394316012b630f
Conrad <conrad.rzuc@gmail.com>
2023-05-17 12:31:50 +0100

    init

--- background.js
+++ background.js
@@ -2,4 +2,6 @@
 browser.browserAction.onClicked.addListener(tab => {
   // Send a message to the content script to read out the subtitles
   browser.tabs.sendMessage(tab.id, { action: 'readSubtitles' });
+
+  browser.runtime.openOptionsPage();
 });
