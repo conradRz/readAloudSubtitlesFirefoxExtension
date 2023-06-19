b8b91f4540c4baf7b50ff0a23a68aac3182a3bf3
Conrad <conrad.rzuc@gmail.com>
2023-06-20 00:07:22 +0100

    Update events.js

--- background/events.js
+++ background/events.js
@@ -20,8 +20,6 @@ var handlers = {
   stop: stop,
   pause: pause,
   getPlaybackState: getPlaybackState,
-  reportIssue: reportIssue,
-  authWavenet: authWavenet,
   getSpeechPosition: function () {
     return getActiveSpeech()
       .then(function (speech) {
@@ -80,7 +78,6 @@ function playText(text, opts) {
   }
   return activeDoc.play()
     .catch(function (err) {
-      handleError(err);
       closeDoc();
       throw err;
     })
@@ -95,7 +92,6 @@ function playTab(tabId) {
   }
   return activeDoc.play()
     .catch(function (err) {
-      handleError(err);
       closeDoc();
       throw err;
     })
@@ -127,7 +123,6 @@ function getActiveSpeech() {
 
 function openDoc(source, onEnd) {
   activeDoc = new Doc(source, function (err) {
-    handleError(err);
     closeDoc();
     if (typeof onEnd == "function") onEnd(err);
   })
@@ -140,100 +135,6 @@ function closeDoc() {
     activeDoc = null;
     silenceLoop.pause();
   }
-}
-
-function handleError(err) {
-  if (err) {
-    var code = /^{/.test(err.message) ? JSON.parse(err.message).code : err.message;
-    if (code == "error_payment_required") clearSettings(["voiceName"]);
-    reportError(err);
-  }
-}
-
-function reportError(err) {
-  if (err && err.stack) {
-    var details = err.stack;
-    if (!details.startsWith(err.name)) details = err.name + ": " + err.message + "\n" + details;
-    getState("lastUrl")
-      .then(function (url) { return reportIssue(url, details) })
-      .catch(console.error)
-  }
-}
-
-function reportIssue(url, comment) {
-  var manifest = browser.runtime.getManifest();
-  return getSettings()
-    .then(function (settings) {
-      if (url) settings.url = url;
-      settings.version = manifest.version;
-      settings.userAgent = navigator.userAgent;
-      return ajaxPost(config.serviceUrl + "/read-aloud/report-issue", {
-        url: JSON.stringify(settings),
-        comment: comment
-      })
-    })
-}
-
-function authWavenet() {
-  createTab("https://cloud.google.com/text-to-speech/#put-text-to-speech-into-action", true)
-    .then(function (tab) {
-      addRequestListener();
-      browser.tabs.onRemoved.addListener(onTabRemoved);
-      return showInstructions();
-
-      function addRequestListener() {
-        browser.webRequest.onBeforeRequest.addListener(onRequest, {
-          urls: ["https://cxl-services.appspot.com/proxy*"],
-          tabId: tab.id
-        })
-      }
-      function onTabRemoved(tabId) {
-        if (tabId == tab.id) {
-          browser.tabs.onRemoved.removeListener(onTabRemoved);
-          browser.webRequest.onBeforeRequest.removeListener(onRequest);
-        }
-      }
-      function onRequest(details) {
-        var parser = parseUrl(details.url);
-        var qs = parser.search ? parseQueryString(parser.search) : {};
-        if (qs.token) {
-          updateSettings({ gcpToken: qs.token });
-          showSuccess();
-        }
-      }
-      function showInstructions() {
-        return executeScript({
-          tabId: tab.id,
-          code: [
-            "var elem = document.createElement('DIV')",
-            "elem.id = 'ra-notice'",
-            "elem.style.position = 'fixed'",
-            "elem.style.top = '0'",
-            "elem.style.left = '0'",
-            "elem.style.right = '0'",
-            "elem.style.backgroundColor = 'yellow'",
-            "elem.style.padding = '20px'",
-            "elem.style.fontSize = 'larger'",
-            "elem.style.zIndex = 999000",
-            "elem.style.textAlign = 'center'",
-            "elem.innerHTML = 'Please click the blue SPEAK-IT button, then check the I-AM-NOT-A-ROBOT checkbox.'",
-            "document.body.appendChild(elem)",
-          ]
-            .join(";\n")
-        })
-      }
-      function showSuccess() {
-        return executeScript({
-          tabId: tab.id,
-          code: [
-            "var elem = document.getElementById('ra-notice')",
-            "elem.style.backgroundColor = '#0d0'",
-            "elem.innerHTML = 'Successful, you can now use Google Wavenet voices. You may close this tab.'"
-          ]
-            .join(";\n")
-        })
-      }
-    })
 }
 
 function authGoogleTranslate() {
